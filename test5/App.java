3c5c2945c1a17d6bdfc6fbf6a8900c462acdc7f4
SYSTEM JAVAN <mahdinarimani110,com>
2022-04-06 16:50:42 +0430

    Add java

---
+++ test5/App.java
@@ -0,0 +1,74 @@
+
+//import java.util.Scanner;
+import java.lang.System;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       // int a=1;
+       /* switch(a) {
+            case 1:
+                System.out.println("a:"+1);
+            case 2:
+                System.out.println("a:"+2);
+                default:
+                    System.out.println("not found");
+            }*/
+           /* switch (a) {
+                case 1:
+                    System.out.println("a"+1);
+                    break;
+                case 2:
+                System.out.println("a"+2);
+                default:
+                System.out.println("not found");
+                    break;
+            }*/
+/*int a=8;
+int b=18;
+if((a==8||b==19)&&a==20){
+    System.out.println("ok");
+}
+System.out.println("not ok");
+*/
+/*int a,b;
+a=20;
+b=15;
+int sum=0;
+sum=a+b;
+System.out.println("a"+a+"b"+b+"="+sum);
+*/
+/*Scanner input=new Scanner(System.in);
+int a,b;
+
+System.out.println("Please Enter num:");
+a=input.nextInt();
+System.out.println("Please Enter num:");
+b=input.nextInt();
+int sum=0;
+sum=a+b;
+System.out.println(a +"+" +b +"="+sum);
+    }*/
+   /* int a=0;
+    while (a<=12) {
+        System.out.println(a);
+        a++;
+        //System.out.println("\n");
+    }*/
+    /*for (int i = 0; i < 20; i++) 
+        System.out.println(i);
+    */
+    /*int a=0;
+    do {
+        System.out.println(a);
+        a++;
+    } while (a<=20);*/
+    /*int a=1;
+    while (a<=10) {
+        int b=a++;
+        int s=a+b;
+        System.out.println(s);
+    }*/
+    System.out.println("hello");
+}
+
+}
